7402c48b351c46bad8345e49357f630e19a78345
88joonyc <joonyc88@gmail.com>
2021-12-02 12:25:22 -0500

    phase_1

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
 
-const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth')
-const { User } = require('../../db/models')
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { handleValidationErrors } = require('../../utils/validation');
+const { User } = require('../../db/models');
 
 const router = express.Router();
+
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('nickname')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a nickname with at least 4 characters.'),
+    check('nickname')
+        .not()
+        .isEmail()
+        .withMessage('Nickname cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long'),
+    handleValidationErrors,
+]
 
 router.get('/',
     restoreUser,
@@ -19,6 +41,7 @@ router.get('/',
 )
 
 router.post('/',
+    validateSignup,
     asyncHandler(async (req, res) => {
         const { email, password, nickname } = req.body;
         const user = await User.signup({ email, nickname, password });
