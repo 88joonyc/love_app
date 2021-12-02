58fc8422af6c7452bdc0b36357d63734cba39de6
88joonyc <joonyc88@gmail.com>
2021-12-02 14:28:09 -0500

    xsrf

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,12 +3,32 @@ const router = express.Router();
 const apiRouter = require('./api');
 
 // test route
-router.get('/hello/world', function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello, World!');
-});
+// router.get('/hello/world', function(req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello, World!');
+// });
 
 router.use('/api', apiRouter);
+
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+
+    router.get('/', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+
+    router.use(express.static(path.resolve('../frontend/build')));
+
+    router.get(/^(?!\/?api).*/, (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+}
 
 
 module.exports = router;
