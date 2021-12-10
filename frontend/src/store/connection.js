b8f5780b5980140abc88c87ea8baf4288e4ef356
88joonyc <joonyc88@gmail.com>
2021-12-10 16:47:11 -0500

    connection_reducer

---
+++ frontend/src/store/connection.js
@@ -0,0 +1,22 @@
+const MAKE_CONNECTION = 'connection/makeConnection';
+const REMOVE_CONNECTION = 'connection/removeConnection';
+
+const initialState = {
+    connection: null
+}
+
+const connectionReducer = (state = initialState, action ) => {
+    let newState
+    switch (action.type) {
+        case MAKE_CONNECTION:
+            newState = Object.assign({}, state)
+            newState.connection = action.payload
+            return newState
+        case REMOVE_CONNECTION:
+            newState = Object.assign({}, state)
+            newState.connection = null
+            return newState
+        default:
+            return state;
+    }
+}
