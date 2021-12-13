5b32549c39ba3fbaa703310fecaa77dd620a31a5
88joonyc <joonyc88@gmail.com>
2021-12-12 21:43:40 -0500

    connect

--- frontend/src/pages/connect/index.js
+++ frontend/src/pages/connect/index.js
@@ -7,7 +7,15 @@ const Connect = () => {
 
     return (
         <div className="connect-body">
+            <div className="connect-box">
+                <div className="connect-code">
 
+                </div>
+                <form className="connect-form">
+                    <input hidden type='text'/>
+                    <input hidden type='text'/>
+                </form>
+            </div>
         </div>
     )
 }
