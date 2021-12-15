669d4fdb4e2843ab087eaccc3caffe23df210177
88joonyc <joonyc88@gmail.com>
2021-12-15 11:50:14 -0500

    connection

--- frontend/src/pages/connect/index.js
+++ frontend/src/pages/connect/index.js
@@ -1,25 +1,41 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+
+import * as connectAction from '../../actions/connection'
 
 import './connect.css'
 
 const Connect = () => {
 
+    const dispatch = useDispatch()
+
     const currentUser = useSelector(state => state.session.user)
+    const connectStatus = useSelector(state => state.connection.connection)
 
     let num = Math.floor(Math.random() * 100000000)
+
+    const handleBreak = () => {
+
+    }
+
+    const handleConnect = () => {
+        dispatch(connectAction.connect({
+            loveyId: currentUser.id,
+            validator: num
+        }))
+    }
 
     return (
         <div className="connect-body">
             <div className="connect-box">
-                <div className="connect-code">
-                    {num}
-                </div>
+                {connectStatus && <div className="connect-code">
+                    {connectStatus?.validator}
+                </div>}
                 <form className="connect-form">
                     <input hidden type='number' value={currentUser?.id}/>
                     <input hidden type='number' value={num}/>
-                    <button className="connect-button">make a new connection</button>
-                    <button className="delete-button">break connection</button>
+                    {!connectStatus && <button onClick={handleConnect} className="connect-button">make a new connection</button>}
+                    <button onClick={handleBreak} className="delete-button">break connection</button>
                 </form>
             </div>
         </div>
