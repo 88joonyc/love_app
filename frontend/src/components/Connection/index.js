12c8659174dc68a5b732c481b3e412e50fc101aa
88joonyc <joonyc88@gmail.com>
2021-12-17 21:43:39 -0500

    connecty

---
+++ frontend/src/components/Connection/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import './Connection.css'
+
+const ConnectForm = () => {
+
+
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+    }
+
+    return (
+        <>
+            <div className="connecter-container">
+                <form onSubmit={e => handleSubmit(e)} className="connecter-form">
+                    <input name='doveyId' type='number' className="connect-input"/>
+                    <button type='submit' className="connect-submit">submit</button>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default ConnectForm
--- frontend/src/pages/connect/index.js
+++ frontend/src/pages/connect/index.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import * as connectAction from '../../actions/connection'
+
+import ConnectForm from "../../components/Connection";
 
 import './connect.css'
 
@@ -11,6 +13,8 @@ const Connect = () => {
 
     const currentUser = useSelector(state => state.session.user)
     const connectStatus = useSelector(state => state.connection.connection)
+
+    const [ conn, toggleConn ] = useState(false)
 
     let num = Math.floor(Math.random() * 100000000)
 
@@ -30,15 +34,17 @@ const Connect = () => {
     return (
         <div className="connect-body">
             <div className="connect-box">
-                {connectStatus && <div className="connect-code">
+                {connectStatus && !connectStatus.doveyId && !conn && <div className="connect-code">
                     {connectStatus?.validator}
                 </div>}
-                <form className="connect-form">
+                {!conn && <form className="connect-form">
                     <input hidden type='number' value={currentUser?.id}/>
                     <input hidden type='number' value={num}/>
                     {!connectStatus && <button onClick={e => handleConnect(e)} className="connect-button">make a new connection</button>}
                     <button onClick={e => handleBreak(e)} className="delete-button">break connection</button>
-                </form>
+                </form>}
+                {conn && <ConnectForm />}
+                <button className="add-dovey" onClick={() => toggleConn(!conn)}>Add connection</button>
             </div>
         </div>
     )
