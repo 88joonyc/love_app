a814fe27c66c1728dfad0aa6bf903a63ef7f07d5
88joonyc <joonyc88@gmail.com>
2022-01-04 10:50:34 -0500

    albums_page

--- frontend/src/pages/album/index.js
+++ frontend/src/pages/album/index.js
@@ -1,15 +1,16 @@
 import React, {useEffect, useState} from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { loadAlbums } from '../../actions/album';
 import ConnectedNavi from '../../components/ConnectedNavi';
 
 const PhotoGallery = () => {
 
+    const connected = useSelector(state => state.connection.connection)
     const dispatch = useDispatch()
 
     useEffect(() => {
-        dispatch(loadAlbums())
+        dispatch(loadAlbums(connected?.id))
     }, [])
 
     return (
